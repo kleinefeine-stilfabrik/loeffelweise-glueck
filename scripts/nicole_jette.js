e0038a8ca5e7eb49ac5632faf054a77460955933
kleinefeine-stilfabrik <nicole.hoelting@googlemail.com>
2021-06-29 18:48:48 +0200

    Artikel zu Nicole & Jette, Geschichten, Leseglück & Löffelfreunde hinzugefügt

--- scripts/nicole_jette.js
+++ scripts/nicole_jette.js
@@ -15,3 +15,30 @@ scrollScript.addEventListener("load", () => {
     });
   });
 });
+
+const sliders = document.querySelectorAll('#textContainer > .item > div.img');
+
+sliders.forEach(sl => {
+  const amount = sl.childElementCount;
+  changeImg(0);
+  const back = document.createElement('span');
+  const next = document.createElement('span');
+  back.innerHTML = `<i class="fas fa-utensil-spoon"></i>`;
+  next.innerHTML = `<i class="fas fa-utensil-spoon"></i>`;
+  // back.innerHTML = `<i class="fas fa-chevron-left"></i>`;
+  // next.innerHTML = `<i class="fas fa-chevron-right"></i>`;
+  back.classList.add('back');
+  next.classList.add('next');
+  sl.appendChild(back);
+  sl.appendChild(next);
+  back.addEventListener('click', () => changeImg(Number(sl.dataset.index)-1));
+  next.addEventListener('click', () => changeImg(Number(sl.dataset.index)+1));
+  function changeImg(index) {
+    if(index >= amount) index -= amount;
+    if(index < 0) index += amount;
+    sl.dataset.index = index;
+    const imgs = sl.querySelectorAll('img');
+    imgs.forEach(img => img.classList.add('hidden'));
+    imgs[index].classList.remove('hidden');
+  }
+});
